b6c42763e8afd256005f71973e4b43be41626445
roy0206 <65231299+roy0206@users.noreply.github.com>
2020-05-17 23:39:16 +0900

    Add files via upload

--- bot_client/시간.py
+++ bot_client/시간.py
@@ -12,10 +12,14 @@ import sys
 import json
 import asyncio
 import os
+import datetime
+from pytz import timezone, utc
 
 async def on_message(message):
     if message.content == "!시간":
+        KST = timezone('Asia/Seoul')
         now = datetime.datetime.now()
+        now.replace(tzinfo=KST)
         await message.channel.send((str(now.year) + "년" + str(now.month) + "월" + str(now.day) + "일" + str(now.hour) + "시" + str(now.minute) + "분 입니다"))
 
 
